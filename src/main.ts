0cc3aa4cfc6b5ce42b77ebc740d4e8ccc463e23c
HaroldOsorioFlores <haroldosorio32@gmail.com>
2024-02-04 20:51:14 -0500

    register serverless app in aws

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,27 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { HttpExceptionFilter } from './utils/exception/http-excepción.filter';
+import { NestFactory } from '@nestjs/core';
+import serverlessExpress from '@vendia/serverless-express';
+import { Callback, Context, Handler } from 'aws-lambda';
+
+import { AppModule } from './app.module';
+
+let server: Handler;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  app.useGlobalFilters(new HttpExceptionFilter());
-  await app.listen(process.env.PORT_NESTJS_CATO);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.init();
+
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp });
 }
-bootstrap();
+
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  server = server ?? (await bootstrap());
+  return server(event, context, callback);
+};
